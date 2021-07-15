a13da4b72068744c4ea3740e117f919ef1408a17
adabney89 <adabney@ncsu.edu>
2021-07-15 14:34:42 -0400

    Modified scopes to see path variable and velangle error. Added script to change gains - KN

---
+++ setGains.m
@@ -0,0 +1,6 @@
+fltCtrl.setPerpErrorVal(.4,'rad');
+fltCtrl.rollMoment.kp.setValue(1.8,'(N*m)/(rad)');
+fltCtrl.rollMoment.ki.setValue(0,'(N*m)/(rad*s)');
+fltCtrl.rollMoment.kd.setValue(1,'(N*m)/(rad/s)');
+fltCtrl.yawMoment.kp.setValue(1,'(N*m)/(rad)');
+fltCtrl.tanRoll.kp.setValue(.25,'(rad)/(rad)');
