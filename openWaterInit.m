009fe8b4cad9e8a611438c2712a481eba677a7a5
adabney89 <adabney@ncsu.edu>
2021-10-22 10:23:37 -0400

    updated submodule and openWaterModel changes -AA

---
+++ openWaterInit.m
@@ -0,0 +1,17 @@
+% Initialization Script
+load('init_Kite.mat')
+load('sStarTable.mat')
+
+fpath = fullfile(fileparts(which('OCTProject.prj')),...
+    'vehicleDesign\Tether\Tension\');
+loadComponent('pathFollowCtrlExp');
+loadComponent('poolScaleKiteAbney');
+loadComponent('poolTether')
+loadComponent('constBoothLem');
+loadComponent('ConstXYZT');
+loadComponent('raftGroundStation');
+loadComponent('constXYZT');
+fltCtrl.initPathVar.setValue(0,'')
+FLIGHTCONTROLLER = 'takeoffToLanding'
+pathEnable = 1;
+
